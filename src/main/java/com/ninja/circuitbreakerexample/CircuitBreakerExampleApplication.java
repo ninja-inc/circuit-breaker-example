8655277e235cd34b63b5b1e4d51f7ab976a3aabe
takaaki.iida <takiida0605@gmail.com>
2019-02-13 16:22:37 +0900

    first commit

---
+++ src/main/java/com/ninja/circuitbreakerexample/CircuitBreakerExampleApplication.java
@@ -0,0 +1,14 @@
+package com.ninja.circuitbreakerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerExampleApplication.class, args);
+	}
+
+}
+
